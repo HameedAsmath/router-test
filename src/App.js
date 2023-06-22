868f7b811660ef1bf8ee37b2196024761efb78b6
HameedAsmath <hameedasmath2001@gmail.com>
2023-06-22 12:41:30 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { createBrowserRouter } from 'react-router-dom';
+
+const App = () => {
+    return (
+      <div className="App">
+        <button>Home</button>
+        <button>AboutUs</button>
+        <button>Contact</button>
+      </div>
+    );
+  }
+export default App;
